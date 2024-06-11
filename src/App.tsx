a0e52f577718c275fb7e291d441fd42c7ff50d29
bryan ruiz <andyruiz2468@gmail.com>
2024-06-11 17:01:40 -0600

    publish my changes to my rep

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Pusher from 'pusher-js';
-import * as PusherPushNotifications from "@pusher/push-notifications-web";
-
-// import { PusherPushNotifications } from "https://js.pusher.com/beams/1.0/push-notifications-cdn.js";
-
-import '../src/coomponents/service-worker'
-
-// import { setPusherClient } from 'react-pusher';
-// import Pusher from 'pusher-js';
-
-// import useSound from 'use-sound';
-// import lol from 'lol.mp3'
-
 
 interface Message {
   username: string;
   message: string;
 }
 
-function App() {
-  // const [playsound] = useSound(lol)
+const App: React.FC = () => {
+  const [username, setUserName] = useState<string>('username');
+  const [messages, setMessages] = useState<Map<string, Message>>(new Map());
+  const [message, setMessage] = useState<string>("");
 
-  const [username, setUserName] = useState('username');
-
-  const [messages, setMessages] = useState<Message[]>([]);
-
-  const [message, setMessage] = useState("");
-  let [allmessages, setAllMessages] = useState<any>([]);
-
-
-  const [num, setNum] = useState<number>(0);
   useEffect(() => {
-    // Enable pusher logging - don't include this in production
-    //  Pusher.logToConsole = true;
-
     const pusher = new Pusher('efc5c18caf9f89f8e460', {
-      cluster: 'mt1'
+      cluster: 'mt1',
+      forceTLS: true,
+      // logToConsole: true,
     });
 
     const channel = pusher.subscribe('banana-666');
-    channel.bind('chat-event', function (data: any) {
-      //  alert(JSON.stringify(data));
-      // setMessages((prevMessages) => [...prevMessages, data]);
-      // console.log(messages);
-      allmessages.push(data);
-      setMessages(allmessages);
-    });
-    setNum(1);
-  }, [])
+    
+    const handleMessage = (data: Message) => {
+      setMessages(prevMessages => {
+        const newMessages = new Map(prevMessages);
+        newMessages.set(data.username, data);
+        return newMessages;
+      });
+    };
 
-  // ---------------  esta funcion manda los datos al server quien usar el 
-  //tigger para mandar los datos al canal y aqui mismo recibirlos en la suscripcion
-  const somit = async (e: any) => {
+    channel.bind('chat-event', handleMessage);
 
-    // console.log("Value :",e);
+    return () => {
+      channel.unbind('chat-event', handleMessage);
+      channel.unsubscribe();
+      pusher.disconnect();
+    };
+  }, []);
+
+  const submit = async (e: React.FormEvent) => {
+    e.preventDefault();
     await fetch('http://localhost:8000/api/messages', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -63,72 +46,43 @@ function App() {
         username: username,
         message: message
       })
-
     });
-
-    // Pusher.trigger('my-channel', 'my-event', {message => 'hello world'})
-    setMessage('')
+    setMessage('');
   }
-  console.log(messages);
-  // console.log(username)
-  // console.log(message)
-
-  //---------------------------------> aqui va lo de las notificaciones
-
-
-
-    
-    // const beamsClient = new PusherPushNotifications.Client({
-    //   instanceId: '6796c8ba-3f42-41a8-85a1-e34dc4eed764',
-    // });
-  
-    // beamsClient.start()
-    //   .then(() => beamsClient.addDeviceInterest('hello'))
-    //   .then(() => console.log('Successfully registered and subscribed!'))
-    //   .catch(console.error);
-    
-
 
   return (
     <div className="App">
-{/* <button className='btn btn-primary' onClick={()=>playsound()} >play</button> */}
-
       <div className='container'>
         <div className="list-group-item list-group-item-action active py-3 lh-sm" aria-current="true">
           <div className="d-flex w-100 align-items-center justify-content-between">
             <strong className="mb-1">List group</strong>
-
           </div>
-          <input className='fs-5 fw-semibold' value={username}
-            onChange={e => setUserName(e.target.value)} />
+          <input
+            className='fs-5 fw-semibold'
+            value={username}
+            onChange={e => setUserName(e.target.value)}
+          />
         </div>
 
         <div className="list-group list-group-flush border-bottom scrollarea">
-
-          {messages.map(message => {
-
-            return (
-              <div className="list-group-item list-group-item-action py-3 lh-sm">
-                <div className="d-flex w-100 align-items-center justify-content-between">
-                  <strong className="mb-1" style={{ color: "black" }}>{message.username}</strong>
-
-                </div>
-                <div className="col-10 mb-1 small">{message.message}</div>
+          {Array.from(messages.values()).map((message, index) => (
+            <div key={index} className="list-group-item list-group-item-action py-3 lh-sm">
+              <div className="d-flex w-100 align-items-center justify-content-between">
+                <strong className="mb-1" style={{ color: "black" }}>{message.username}</strong>
               </div>
-            )
-          })}
-
-
+              <div className="col-10 mb-1 small">{message.message}</div>
+            </div>
+          ))}
         </div>
       </div>
-      <form onSubmit={e => somit(e)}>
-        <input className='form-control' placeholder='wirte a mesagge' value={message}
+      <form onSubmit={submit}>
+        <input
+          className='form-control'
+          placeholder='Write a message'
+          value={message}
           onChange={e => setMessage(e.target.value)}
         />
-
-        {/* <button className='btn btn-primary'>Send</button> */}
       </form>
-
     </div>
   );
 }
--- src/assets/sonidos/audio.ts
+++
@@ -1,6 +0,0 @@
-exports.songsdata= [
-    {
-        "title": "Bad-bunny",
-        "url": ""
-    }
-]
